8a01cdc3952b9fa65e6f9b98d75885f831562d71
Samuele Ingrassia <samuele.ingrassia@studenti.ittsrimini.edu.it>
2022-04-05 12:05:38 +0200

    Aggiunto test NUnit

---
+++ AS2122_3G_INF_IngraSamuele_CalcolatriceDLL.Test/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace AS2122_3G_INF_IngraSamuele_CalcolatriceDLL.Test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
